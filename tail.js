8b180725139442825d433598ac3b27e33c30991f
Calvycoco <su.calvin@live.com>
2023-07-24 21:42:35 -0700

    Passing tail Assertions

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let newArr = arr.slice(1);
+  return newArr; 
+};
+
+const oneArr = [];
+const result = tail(oneArr);
+console.log(assertEqual(result[0], undefined));
+console.log(assertEqual(oneArr.length, 0));

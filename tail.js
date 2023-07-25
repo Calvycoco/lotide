7b2fbc06378b67864e47f31e41f2a8551ba6624d
Calvycoco <su.calvin@live.com>
2023-07-24 22:48:11 -0700

    Passing tail Assertion

--- tail.js
+++ tail.js
@@ -11,7 +11,7 @@ const tail = function(arr) {
   return newArr;
 };
 
-const oneArr = [];
-const result = tail(oneArr);
-console.log(assertEqual(result[0], undefined));
-console.log(assertEqual(oneArr.length, 0));
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"

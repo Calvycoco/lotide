338c0e6dc910b97803397266dba16a1114f5f247
Calvycoco <su.calvin@live.com>
2023-07-25 21:22:26 -0700

    Pass or failing arrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 function eqArrays(arr1, arr2) {
   let test = true; //If true then outcome will be true
   if (arr1.length !== arr2.length) { //This statement is to check if the two arrays has different lengths.
-    return test; 
+    return false; 
   }
   for (let i = 0; i < arr1.length; i++) { // Loop through the arrays.
     if (arr1[i] !== arr2[i]) { // To see if the contents within the arrays has the same lengths.

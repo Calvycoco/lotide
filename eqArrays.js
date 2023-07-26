1e2d39b5e2329b1d2a4f605449d7adab94a800be
Calvycoco <su.calvin@live.com>
2023-07-25 20:51:12 -0700

    Pass or failing arrays

--- eqArrays.js
+++ eqArrays.js
@@ -4,16 +4,13 @@ const assertEqual = function(actual, expected) {
   } else {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
-
 //Wanted to attempt return statements for more legibility and less cluster, SUCCESS!
 function eqArrays(arr1, arr2) {
   let test = true; //If true then outcome will be true
   if (arr1.length !== arr2.length) { //This statement is to check if the two arrays has different lengths.
     return test; 
   }
-
   for (let i = 0; i < arr1.length; i++) { // Loop through the arrays.
     if (arr1[i] !== arr2[i]) { // To see if the contents within the arrays has the same lengths.
       test = false;
@@ -22,9 +19,7 @@ function eqArrays(arr1, arr2) {
   }
   return test //Check to see if there are differences.
 }
-
 // Testing
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [2, 1, 3]), false); // => false
 assertEqual(eqArrays([1, 2, 3], [2, 3, 1]), false); // => false

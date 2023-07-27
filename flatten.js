8bd0f679e04022a9e42137a0af5aac0d0c22200b
Calvycoco <su.calvin@live.com>
2023-07-26 18:44:08 -0700

    flattened

--- flatten.js
+++ flatten.js
@@ -1,6 +1,6 @@
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-  console.log(`✅ Success!: ${arr1} === ${arr2}`);
+    console.log(`✅ Success!: ${arr1} === ${arr2}`);
   } else {
     console.log(`❌ Failed!: ${arr1} !== ${arr2}`);
   }
@@ -9,7 +9,7 @@ const assertArraysEqual = function(arr1, arr2) {
 function eqArrays(arr1, arr2) {
   let test = true; //If true then outcome will be true
   if (arr1.length !== arr2.length) { //This statement is to check if the two arrays has different lengths.
-    return false; 
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) { // Loop through the arrays.
     if (arr1[i] !== arr2[i]) { // To see if the contents within the arrays has the same lengths.
@@ -17,8 +17,8 @@ function eqArrays(arr1, arr2) {
       return test;
     }
   }
-  return test //Check to see if there are differences.
-};
+  return test; //Check to see if there are differences.
+}
 
 function flatten(arr) {
   if (!Array.isArray(arr)) {

dc65942d9c22ce29738b386e9bcceef4cc3e5a6b
Calvycoco <su.calvin@live.com>
2023-07-31 15:15:12 -0700

    revising for tests

--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let newArr = arr.slice(1);
   return newArr;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+module.exports = tail;
